0cb1078034f680e27f5c97c4630b34139e0a542d
MehmoodAkmal <akmalbajwa2672@gmail.com>
2026-09-20 14:25:17 +0500

    chore(db): update mongodb connection URI environment variable reference

--- src/db/db.js
+++ src/db/db.js
@@ -2,16 +2,16 @@ import mongoose from "mongoose";
 import { createSuperAdmin } from "./createSuperAdmin.js";
 import User from "../Schemas/auth.js";
 const dbConnection = async () => {
-    try{
-         await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/inventory");
-         console.log("Database Connected")
-         const user = await User.findOne({role: "superAdmin"});
-         if (!user) {
-            await createSuperAdmin()
-         }
-    }catch(error){
-         console.log(error.message);
-    }
+     try {
+          await mongoose.connect(process.env.MONGODB_URI);
+          console.log("Database Connected")
+          const user = await User.findOne({ role: "superAdmin" });
+          if (!user) {
+               await createSuperAdmin()
+          }
+     } catch (error) {
+          console.log(error.message);
+     }
 }
 
 export default dbConnection;
